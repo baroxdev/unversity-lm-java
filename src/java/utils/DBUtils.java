510ce71c6cf061cef8842542f4bbff2665281e1d
Barox Phan <75582570+baroxFPTU@users.noreply.github.com>
2023-04-03 14:32:54 +0700

    update db password

--- src/java/utils/DBUtils.java
+++ src/java/utils/DBUtils.java
@@ -15,8 +15,10 @@ public class DBUtils {
 
     public static Connection makeConnection() throws Exception {
         Connection cn = null;
+
         String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-        String url = "jdbc:sqlserver://plantee.database.windows.net:1433;database=ULMDatabase;user=baroxdev@plantee;password=Bao19082002.;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30";
+        String password = "Bao19082002.";
+        String url = "jdbc:sqlserver://plantee.database.windows.net:1433;database=ULMDatabase;user=baroxdev@plantee;password=" + password + ";encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30";
         Class.forName(driver);
         cn = DriverManager.getConnection(url);
         return cn;
